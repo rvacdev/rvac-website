77bd07bd791fd2d448580f9544a81825439da3c9
Aubreye Freeman <pjf0831@yahoo.com>
2024-01-04 15:46:30 -0600

    modified:   cart.html 	modified:   scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,12 @@
 (function(){
+    
 
+    function changeStuff(){
+        document.getElementById("change").innerHTML="Hello JavaScript!";
+    }
 
+    toggleCartBtn.addEventListener("click", function(e) {
+        e.preventDefault();
+        changeStuff();
+      });
 })();
