79494b949dc167388e0c77b598f5dfb264bc1b58
Aubreye Freeman <pjf0831@yahoo.com>
2024-03-15 13:40:15 -0500

    modified:   client/main.py 	modified:   client/templates/admin.html

--- client/main.py
+++ client/main.py
@@ -1,11 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user
+
 app = Flask(__name__)
 
-@app.route('/')
+#CHANGE LOCATION OF DATBASE TO CONNECT TO THE ACCTUAL DATABASE
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+
+#EDIT THE SECRET KEY
+app.config['SECRET_KEY']='ENTER SECRET KEY'
+
+db=SQLAlchemy()
+
+manager=LoginManager()
+manager.init_app(app)
+
+class Users(UserMixin,db.Model):
+  #CHANGE THESE VALUES TO MATCH OUR DATABASE
+  id=db.column(db.String(250), unique=True,
+                         nullable=False)
+  password = db.Column(db.String(250),
+                         nullable=False)
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+@manager.user_loader
+def loader_user(userID):
+   return Users.query.get(userID)
+
+#Display Webpage
+
+@app.route('/admin', methods=["GET",'POST'])
 def admin():
+  if request.method=='POST':
+     user=Users.query.filter_by(username=request.form.get("username")).first()
+     if user.password==request.form.get('password'):
+        login_user(user)
+        return render_template('admin.html')
   return render_template('admin.html')
 
-@app.route("/index")
+@app.route("/")
 def index():
     return render_template('index.html')
 
